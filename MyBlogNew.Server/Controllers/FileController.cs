a96da84c2ed1160a19cc26653195d8ca4719cb33
Daemon Walker <daemon0525@163.com>
2019-04-26 17:01:13 +0800

    v1.0 大概能用了。。。

---
+++ MyBlogNew.Server/Controllers/FileController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogNew.Server.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult DownloadCode()
+        {
+            return File("/resources/MyBlogNew-master.zip", "application/x-zip-compressed", "MyBlogNew-master.zip", true);
+        }
+    }
+}
